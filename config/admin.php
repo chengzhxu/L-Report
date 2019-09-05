7c26d832e0b82f2701b7fa6b86875c237fb5f101
chengzhaoxu <chngzhxu@163.com>
2019-09-05 17:09:01 +0800

    jenkins test master

--- config/admin.php
+++ config/admin.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Laravel-admin name
+    | Laravel-admin name2
     |--------------------------------------------------------------------------
     |
     | This value is the name of laravel-admin, This setting is displayed on the
